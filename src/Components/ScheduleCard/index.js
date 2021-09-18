00929b50073246b9e4932ba7e25996b68184bdec
ImadChughtai <41489373+ImadChughtai@users.noreply.github.com>
2021-09-18 19:09:52 +0500

    ..

---
+++ src/Components/ScheduleCard/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./index.css"
+
+const ScheduleCard = () => {
+    
+    return (
+        <div className="sCardMainDiv">
+
+
+
+            <div className="sCardLeftDiv">
+
+            </div>
+
+            <div>
+
+
+            </div>
+
+        </div>
+    );
+};
+
+export default ScheduleCard;
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 import Sidebar from "./Sidebar/index";
 import Header from "./Header/index";
-export { Sidebar, Header };
+import ScheduleCard from "./ScheduleCard";
+export { Sidebar, Header, ScheduleCard };
