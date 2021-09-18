6b2490680c2d9ab1fc85712ddb7682e415314312
smkh <smkh.kashan@gmail.com>
2021-09-18 19:11:08 +0500

    Update AddSchedule.js

--- src/Screen/AddSchedule.js
+++ src/Screen/AddSchedule.js
@@ -7,7 +7,7 @@ import Autocomplete from "@mui/material/Autocomplete";
 
 const useStyles = makeStyles({
   paper: {
-    padding: 10,
+    padding: 20,
   },
 });
 const AddSchedule = () => {
@@ -18,7 +18,7 @@ const AddSchedule = () => {
       <Header title="Add New Schedule" />
       <div className="content-container">
         <Paper variant="outlined" className={classes.paper}>
-          <Grid container spacing={1}>
+          <Grid container spacing={2}>
             <Grid item xs={6}>
               <Autocomplete
                 disablePortal
@@ -34,12 +34,15 @@ const AddSchedule = () => {
               <Autocomplete
                 disablePortal
                 id="combo-box-demo"
-                options={doctorsArr}
+                options={availabillityArr}
                 renderInput={(params) => (
-                  <TextField {...params} label="time available" fullWidth/>
+                  <TextField {...params} label="time available"/>
                 )}
                 fullWidth={true}
               />
+            </Grid>
+            <Grid item xs={6}>
+             
             </Grid>
           </Grid>
         </Paper>
@@ -62,16 +65,14 @@ const doctorsArr = [
   
 ];
 
-const doctorsAr = [
-    { label: "Doctor A", year: 1994 },
-    { label: "Doctor B", year: 1994 },
-    { label: "Doctor C", year: 1994 },
-    { label: "Doctor D", year: 1994 },
-    { label: "Doctor E", year: 1994 },
-    { label: "Doctor F", year: 1994 },
-    { label: "Doctor G", year: 1994 },
-    { label: "Doctor H", year: 1994 },
-  
-    
+const availabillityArr = [
+    { label: "on monday 12:00 am to 13:00 pm", year: 1994 },
+    { label: "on tuesday 12:00 am to 13:00 pm", year: 1994 },
+    { label: "on wednesday 12:00 am to 13:00 pm", year: 1994 },
+    { label: "on thursday 12:00 am to 13:00 pm", year: 1994 },
+    { label: "on friday 12:00 am to 13:00 pm", year: 1994 },
+    { label: "on saturday 12:00 am to 13:00 pm", year: 1994 },
+    { label: "on sunday 12:00 am to 13:00 pm", year: 1994 },
+
   ];
 export default AddSchedule;
