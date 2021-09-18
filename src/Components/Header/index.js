9bce0235ff039e581bb047d0438649e23e2bb0af
smkh <smkh.kashan@gmail.com>
2021-09-18 18:57:02 +0500

    add schedule inprogress

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,30 +1,33 @@
 import React from "react";
 
-import { Typography } from "@mui/material";
+import { Typography, Paper } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import "./header.css";
 
 const Header = ({ title, rightComponent }) => {
   return (
-    <div className="header-container">
-      <div className="header-child">
-        <Typography variant="h4">{title}</Typography>
-      </div>
-      <div className="header-right-container">
-        {Boolean(rightComponent) && rightComponent}
-        <div className="header-right">
-          <Avatar
-            alt="Remy Sharp"
-            src="/static/images/avatar/1.jpg"
-            sx={{ width: 45, height: 45 }}
-          />
-          <div className="user-details">
-            <Typography variant="subtitle2">ABDUL HASEEB</Typography>
-            <Typography variant="caption">ONLINE</Typography>
+    <>
+      <div className="header-container">
+        <div className="header-child">
+          <Typography variant="h4">{title}</Typography>
+        </div>
+        <div className="header-right-container">
+          {Boolean(rightComponent) && rightComponent}
+          <div className="header-right">
+            <Avatar
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: 45, height: 45 }}
+            />
+            <div className="user-details">
+              <Typography variant="subtitle2">ABDUL HASEEB</Typography>
+              <Typography variant="caption">ONLINE</Typography>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* <Paper variant="outlined"></Paper> */}
+    </>
   );
 };
 
--- src/Screen/AddSchedule.js
+++ src/Screen/AddSchedule.js
@@ -1,16 +1,77 @@
 import React, { useState } from "react";
 
-import { Container, Typography } from "@mui/material";
+import { Container, TextField, Paper, Grid } from "@mui/material";
 import { Header } from "./../Components";
+import { createStyles, makeStyles } from "@mui/styles";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const useStyles = makeStyles({
+  paper: {
+    padding: 10,
+  },
+});
 const AddSchedule = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <Header title="Add New Schedule" />
       <div className="content-container">
-        <Typography>AddSchedule</Typography>
+        <Paper variant="outlined" className={classes.paper}>
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={doctorsArr}
+                renderInput={(params) => (
+                  <TextField {...params} label="Doctor / Therapist" fullWidth/>
+                )}
+                fullWidth={true}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={doctorsArr}
+                renderInput={(params) => (
+                  <TextField {...params} label="time available" fullWidth/>
+                )}
+                fullWidth={true}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
       </div>
     </div>
   );
 };
 
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const doctorsArr = [
+  { label: "Doctor A", year: 1994 },
+  { label: "Doctor B", year: 1994 },
+  { label: "Doctor C", year: 1994 },
+  { label: "Doctor D", year: 1994 },
+  { label: "Doctor E", year: 1994 },
+  { label: "Doctor F", year: 1994 },
+  { label: "Doctor G", year: 1994 },
+  { label: "Doctor H", year: 1994 },
+
+  
+];
+
+const doctorsArr = [
+    { label: "Doctor A", year: 1994 },
+    { label: "Doctor B", year: 1994 },
+    { label: "Doctor C", year: 1994 },
+    { label: "Doctor D", year: 1994 },
+    { label: "Doctor E", year: 1994 },
+    { label: "Doctor F", year: 1994 },
+    { label: "Doctor G", year: 1994 },
+    { label: "Doctor H", year: 1994 },
+  
+    
+  ];
 export default AddSchedule;
