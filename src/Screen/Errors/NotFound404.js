8680737c2b3d7e419ab26e88f08b9bf1f5239194
KASHAN HAIDER <smkh.kashan@gmail.com>
2021-09-19 21:08:29 +0500

    Update NotFound404.js

--- src/Screen/Errors/NotFound404.js
+++ src/Screen/Errors/NotFound404.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound404 = () => {
     return (
         <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: window.innerHeight / 2 }}>
             <div>
-                <img style={{maxWidth:'100%'}} src={require('../../Assets/Images/404.png').default} />
+                <img style={{ maxWidth: '80%' }} src={require('../../Assets/Images/404.png').default} />
                 <p style={{ textAlign: 'center' }}>404 NOT FOUND!</p>
+
+                <Link to="/">
+                    <p style={{ fontSize: 30, textAlign: 'center' }}>BACK TO HOME</p>
+                </Link>
             </div>
         </div>
     )
